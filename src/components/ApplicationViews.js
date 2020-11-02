80f933b04cf2b9ab58f14d63a12d0852d8e40bd6
heymonicakay <hey.monicakay@gmail.com>
2020-11-02 16:00:56 -0600

    first commit with games showing upon authentication.

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { GameProvider } from "./game/GameProvider"
+import { GameList } from "./game/GameList"
+
+
+export const ApplicationViews = () => {
+    return <>
+        <main style={{
+            margin: "5rem 2rem",
+            backgroundColor: "lightgoldenrodyellow"
+        }}>
+            <GameProvider>
+                <Route exact path="/">
+                    <GameList />
+                </Route>
+            </GameProvider>
+            Application views
+        </main>
+    </>
+}
